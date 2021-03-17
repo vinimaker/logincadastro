b00c79a5ffa05109557f3c74f761784f024dfdd7
vinimaker <79667607+vinimaker@users.noreply.github.com>
2021-03-17 15:51:38 -0300

    Add files via upload

---
+++ login-php/index.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Login - Projeto  - Vinicius e Matheus A.</title>
+   
+</head>
+
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3>Sistema de Login</h3>
+                    <h3>Vinicius e Matheus A.</h3>
+                    <?php
+                    if(isset($_SESSION['nao_autenticado'])):
+                    ?>
+                    <div class="notification is-danger">
+                      <p>ERRO: Usuário ou senha inválidos.</p>
+                    </div>
+                    <?php
+                    endif;
+                    unset($_SESSION['nao_autenticado']);
+                    ?>
+                    <div class="box">
+                        <form action="login.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <a href="cadastro.php">Cadastrar</a>
+                            </div>
+                            <button type="submit">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
